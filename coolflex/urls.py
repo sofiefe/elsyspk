e293403c24471b070470ff0cb20fe55a5cd3cc6d
sofiefe <sofiefe@stud.ntnu.no>
2022-03-07 11:15:34 +0100

    la til user og forms.py

--- coolflex/urls.py
+++ coolflex/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.home),
+  #path("", views.home),
   path("cooluser/", views.CoolUserList.as_view(), name="cooluserlist"),
   path("design/<pk>", views.DesignList.as_view(), name="designlist"),
+  path("", views.UserFormCreate.as_view(), name="createuser")
 
 ]
--- coolflex/views.py
+++ coolflex/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 import random
-from .models import CoolUser, Design
+from .models import CoolUser, Design, User
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import UserForm
 
 # Create your views here.
 def home(request):
@@ -22,14 +23,14 @@ class CoolUserList(ListView):
     model = CoolUser
     template = 'coolflex/home.html'
 
-    #def head(self, *args, **kwargs):
-        #user_list = self.all().order_by("first_name")
-        #response = HttpResponse(user_list)
+   
 
 class DesignList(ListView):
     model = Design
     template = 'coolflex/design.html'
 
-    #def head(self, *args, **kwargs):
-        #design_list = self.get_queryset()
-        #response = HttpResponse(design_list)
+
+class UserFormCreate(CreateView):
+    model = User
+    template_name = "cool/userform_template.html"
+    form_class = UserForm
